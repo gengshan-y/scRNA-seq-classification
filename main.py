33b60bebaf197789d7df2fa25cd3c9819ca42d4f
Gengshan Yang <gengshay@compute-0-14.local>
2017-11-28 23:04:09 -0500

    before change back

--- main.py
+++ main.py
@@ -15,20 +15,21 @@ display_step = 100
 save_step = 10000
 
 # Network Parameters
-num_hidden_1 = 1 # 1st layer num features
-num_hidden_2 = 1 # 2nd layer num features (the latent dim)
-num_input = 20463 # MNIST data input (img shape: 28*28)
+num_input = 20463 
 
 BATCH_SIZE=100
 log_directory = '..'
 INPUT_PATH = os.path.join('..', 'data')
-VAL_ITER = 30
+VAL_ITER = 40  # cover all validation set
 
 import argparse
 parser = argparse.ArgumentParser(description='.')
 parser.add_argument('--checkpoint_path',           type=str,   help='path to a specific checkpoint to load', default='')
+parser.add_argument('--data_name',           type=str,   help='path to a specific checkpoint to load', default='')
 args = parser.parse_args()
 
+train_name = 'train' + args.data_name + '.tfrecords' 
+val_name = 'val' + args.data_name + '.tfrecords'
 
 def create_dataloader(datapath, batch_size):
     filename_queue = tf.train.string_input_producer([datapath])
@@ -49,35 +50,47 @@ def create_dataloader(datapath, batch_size):
     
 
 data_batch_train, label_batch_train = create_dataloader(os.path.join(INPUT_PATH,\
-                                               'train.tfrecords'), BATCH_SIZE)
+                                               train_name), BATCH_SIZE)
 data_batch_val, label_batch_val = create_dataloader(os.path.join(INPUT_PATH,\
-                                               'val.tfrecords'), BATCH_SIZE)
+                                               val_name), BATCH_SIZE)
 #X = tf.placeholder(tf.float32, shape=(BATCH_SIZE, num_input))
 #y_true = tf.placeholder(tf.int64, shape=(BATCH_SIZE))
-X = data_batch_train
-y_true = label_batch_train
+
 # Construct model
-fc1 = tf.contrib.layers.fully_connected(X, 1024)
-fc2 = tf.contrib.layers.fully_connected(fc1, 100)
-fc3 = tf.contrib.layers.fully_connected(fc1, 46)
-#fc1 = tf.contrib.layers.fully_connected(X, 1024, activation_fn=tf.nn.elu)
-#fc2 = tf.contrib.layers.fully_connected(fc1, 100, activation_fn=tf.nn.elu)
-#fc3 = tf.contrib.layers.fully_connected(fc1, 46, activation_fn=tf.nn.sigmoid, normalizer_fn = slim.batch_norm)
-loss = tf.nn.softmax_cross_entropy_with_logits(logits=fc3,\
-                  labels=tf.one_hot(y_true,46))
-loss = tf.reduce_mean(loss)
-y_pred = tf.nn.softmax(fc3)
-y_pred = tf.argmax(y_pred,axis=-1)
+def build_model(X,y_true,is_train=True,reuse=None,scope='model'):
+    #with tf.variable_scope(scope, 'my_model' ,[X,y_true], reuse=reuse):
+    with tf.variable_scope(scope, reuse=reuse):
+        fc1 = tf.contrib.layers.fully_connected(X, 1024)
+        fc2 = tf.contrib.layers.fully_connected(fc1, 100)
+        fc3 = tf.contrib.layers.fully_connected(fc2, 46)
+        #fc1 = tf.contrib.layers.fully_connected(X, 1024, activation_fn=tf.nn.elu,normalizer_fn = slim.batch_norm)
+        #fc2 = tf.contrib.layers.fully_connected(fc2, 100, activation_fn=tf.nn.elu,normalizer_fn = slim.batch_norm)
+        #fc3 = tf.contrib.layers.fully_connected(fc1, 46, activation_fn=tf.nn.sigmoid, normalizer_fn = slim.batch_norm)
+        y_pred = tf.nn.softmax(fc3)
+        pdb.set_trace()
+        y_pred = tf.argmax(y_pred,axis=-1)
+        loss = tf.nn.softmax_cross_entropy_with_logits(logits=fc3,\
+                      labels=tf.one_hot(y_true,46))
+        loss = tf.reduce_mean(loss)
+
+        # build summaries
+        if is_train:
+            sum_name = 'train'
+        else:
+            sum_name = 'val'
+        tf.summary.histogram('X', X, [sum_name])
+        tf.summary.histogram('fc1', fc1, [sum_name])
+        tf.summary.histogram('fc2', fc2, [sum_name])
+        tf.summary.histogram('fc3', fc3, [sum_name])
+        tf.summary.histogram('y_pred', y_pred, [sum_name])
+        tf.summary.scalar('loss', loss, [sum_name])
+    return y_pred, loss, y_true
+
+y_pred,loss,_ =  build_model(data_batch_train,label_batch_train)
+y_pred_v,loss_v,y_true_v =  build_model(data_batch_val,label_batch_val,is_train=False,reuse=True)
 
 optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(loss)
 # optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
-
-tf.summary.histogram('X', X, ['model'])
-tf.summary.histogram('fc1', fc1, ['model'])
-tf.summary.histogram('fc2', fc2, ['model'])
-tf.summary.histogram('fc3', fc3, ['model'])
-tf.summary.histogram('y_pred', y_pred, ['model'])
-tf.summary.scalar('loss', loss, ['model'])
 
 # Start Training
 # Start a new TF session
@@ -85,12 +98,12 @@ config = tf.ConfigProto(allow_soft_placement=True)
 config.gpu_options.allow_growth=True
 config.gpu_options.visible_device_list='3'
 
-summary_op = tf.summary.merge_all('model')
+summary_op = tf.summary.merge_all('train')
+summary_op_v = tf.summary.merge_all('val')
 train_saver = tf.train.Saver()
 
 with tf.Session(config=config) as sess:
     summary_writer = tf.summary.FileWriter(log_directory + '/' + 'log', sess.graph)
-
     # Run the initializer
     sess.run(tf.global_variables_initializer())
     coord = tf.train.Coordinator()
@@ -101,25 +114,22 @@ with tf.Session(config=config) as sess:
     # Training
     for i in range(0, num_steps):
         # Prepare Data
-        _, l = sess.run([optimizer, loss])
-        #_, l = sess.run([optimizer, loss],feed_dict={
-        #              X:data_batch_train.eval(), 
-        #         y_true:label_batch_train.eval()})
+        _, l = sess.run([optimizer, loss])  # dont use eval()
         # Display logs per step
+        l_vs = []; corr_num = 0
         if i % display_step == 0:
-            print('Step %i: Minibatch Loss: %f' % (i, l))
-            #corr_num = 0
-            #for j in range(VAL_ITER):
-            #    numy_true,numy_pred = sess.run([y_true,y_pred],feed_dict={
-            #                X:data_batch_val.eval(),
-            #           y_true:label_batch_val.eval()})
-            #    corr_num += np.sum(numy_true == numy_pred)
-            #acc_show = float(corr_num)/(VAL_ITER*BATCH_SIZE)
-            #print('Step %i: Minibatch Loss: %f, VAL acc: %f' % (i, l, acc_show))
-            #summary_str = sess.run(summary_op, feed_dict={
-            #          X:data_batch_train.eval(),
-            #     y_true:label_batch_train.eval()})
-            #summary_writer.add_summary(summary_str, global_step=i)
+            for j in range(VAL_ITER):
+                l_v,ypv,ytv = sess.run([loss_v, y_pred_v, y_true_v])
+                corr_num += np.sum(ypv == ytv)
+                l_vs.append(l_v)
+            l_val = np.mean(l_vs)
+            acc_v = float(corr_num) / (BATCH_SIZE * VAL_ITER)
+            print('Step %i: Minibatch Loss: %f, VAL loss/acc: %f/%f '\
+                              % (i, l, l_val, acc_v))
+            summary_str = sess.run(summary_op)
+            summary_writer.add_summary(summary_str, global_step=i)
+            summary_str = sess.run(summary_op_v)
+            summary_writer.add_summary(summary_str, global_step=i)
         if i and i % save_step == 0:
             train_saver.save(sess, log_directory + '/model', global_step=i)
     train_saver.save(sess, log_directory + '/model', global_step=i)
